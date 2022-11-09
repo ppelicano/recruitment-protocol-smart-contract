b86e09cc61d93c3ea784eabf9d4dd751c3d6c77b
Pedro Pelicano <pedro.pelicano@lynkco.com>
2022-11-09 21:51:32 +0000

    upgraded to pragma ^0.8.0 #1

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,7 @@ const RegistryMock = artifacts.require('./EnsRegistryMock.sol');
 const ResolverMock = artifacts.require('./EnsResolverMock.sol');
 const EnsSubdomainFactory = artifacts.require('./EnsSubdomainFactory.sol');
 const Recruitment = artifacts.require('./Recruitment.sol');
+const Namehash = require('eth-ens-namehash-ms');
 
 module.exports = async function(deployer, network, accounts) {
 	deployer.then(async () => {
